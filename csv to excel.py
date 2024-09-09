3f6aac071a1ab93d7fb8bb9f156a5c713da0416e
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-09-09 11:22:01 -0600

    feat(add): encoding

--- csv to excel.py
+++ csv to excel.py
@@ -2,9 +2,9 @@
 
 import pandas as pd
 
-archivo = pd.read_csv('data.csv')
+archivo = pd.read_csv('data.csv',encoding='utf-8')
 df = pd.DataFrame(archivo)
 
-GFG = pd.ExcelWriter('data.xlsx')
+GFG = pd.ExcelWriter('data.xlsx', engine='openpyxl')
 df.to_excel(GFG, index=False)
 GFG.close()
